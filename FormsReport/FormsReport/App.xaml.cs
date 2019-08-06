0d2959fe6abb83d91cbec87dc77f974ba15bb276
Dino Angelo Reyes <d1no_ang3lo@hotmail.com>
2019-08-06 20:33:07 +0800

    Test Commit

---
+++ FormsReport/FormsReport/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FormsReport
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var page = new MainPage();
+            MainPage = new NavigationPage(page);
+            NavigationPage.SetHasNavigationBar(page, false);
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ FormsReport/FormsReport/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FormsReport
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void SwitchButton_Clicked(object sender, EventArgs e)
+        {
+            var page = new Switch();
+            await Navigation.PushAsync(page);
+        }
+    }
+}
